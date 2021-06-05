36a76f4f6d0d193144b6c648fc84117fde9eb227
Lisa-Stats <lisa.stathopulos@gmail.com>
2021-06-04 21:04:33 -0400

    Add ideas to sublist

--- sublist/src/sublist.clj
+++ sublist/src/sublist.clj
@@ -7,6 +7,15 @@
 (defn sublist2 [list2]
   (into #{} list2))
 
+(defn checks
+  [vec1 vec2]
+  (if (or (some nil?
+             (for [a vec1 b [vec2]]  (some #(= % a) b)))
+       (some nil?
+             (for [a vec2 b [vec1]]  (some #(= % a) b))))
+    false
+    true))
+
 
 (defn classify [list1 list2]
   (cond
@@ -14,5 +23,3 @@
     (= true (s/subset? (sublist1 list1) (sublist2 list2))) :sublist
     (= false (s/subset? (sublist1 list1) (sublist2 list2))) :superlist
     :else :unequal))
-
-(s/subset? #{1 2} #{1 2 3})
